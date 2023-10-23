cb10d666f4ef62729c3c91af5737098055dbf8c8
Petr-ZN <144950683+Petr-ZN@users.noreply.github.com>
2023-10-23 17:43:49 +0200

    lec_2

---
+++ lec_2_range.py
@@ -0,0 +1,14 @@
+'''for i range(start, stop, step):
+
+''' 
+a = range(0,10,2)
+print (a)
+print (type(a))
+print(a[3])
+
+a = 'good'
+for i in range(0, 11, 1):
+    if i<len(a):
+        print(a[i]+' - Bad')
+    else:
+        print(f'{i}'+' - Good')
---
+++ lec_2_while.py
@@ -0,0 +1,14 @@
+i =5
+while i<15:
+    print("i: ", i)
+    i += 1
+
+for i in 'helo world':
+    if i == 'o':
+        break
+    print(i)
+
+for i in 'helo world':
+    
+        
+    print(i)
