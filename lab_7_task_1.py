cfbefa5cb7a22dd3275a64a0e32f7c863e5bc836
Petr-ZN <144950683+Petr-ZN@users.noreply.github.com>
2024-01-15 16:44:28 +0200

    исправлена ошибка с циклоидой

--- lab_7_task_1.py
+++ lab_7_task_1.py
@@ -4,11 +4,19 @@ import math
 
 def cicloida (r = 1):
     t = np.arange (0, 10, 0.1)
-    x = r * (t - np.sin(t)**3)
-    y = r * (1 - np.cos(t)**3)
+    x = r * (t - np.sin(t))
+    y = r * (1 - np.cos(t))
     
     plt.plot(x, y)
     plt.show()
     
+def astroida (r = 1):
+    t = np.arange (0, 10, 0.1)
+    x = r * np.sin(t)**3
+    y = r * np.cos(t)**3
+    
+    plt.plot(x, y)
+    plt.show()
 if __name__ == '__main__':
-    cicloida(2)
+    #cicloida(2)
+    astroida(-1)
