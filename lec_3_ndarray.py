d612f99be41c01384aeab9e41231f8009a2d020a
Petr-ZN <144950683+Petr-ZN@users.noreply.github.com>
2024-10-11 19:21:30 +0200

    повторение

--- lec_3_ndarray.py
+++ lec_3_ndarray.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+c = np.ndarray((2, 3))
+print(c)
+
 a = np.zeros((2, 3))
 print (a)
 
@@ -10,5 +13,6 @@ b = np.ones((3,2))
 print (b)
 
 d = np.ndarray((3,3))
+
 print(d)
 print(type(d[0,0]))
--- lec_3_numpy_generators.py
+++ lec_3_numpy_generators.py
@@ -1,2 +1,3 @@
 import numpy as np
-
+#np.arange(start, stop, step)
+#np.linspace(start, stop, num) num - число делений
